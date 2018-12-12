dc249ec7dbab8ab2f8c4badb529fce684348b5e0
aaronsg4 <aaronsg4@yahoo.com>
2018-12-11 19:15:04 -0500

    Feature 2 init

--- Branching/Program.cs
+++ Branching/Program.cs
@@ -12,6 +12,8 @@ namespace Branching
         {
             Console.WriteLine("Changes to master");
             Console.WriteLine("More changes to master");
+
+            Console.WriteLine("This is the beginning of the feature 2 branch, which was added after the first feature, and after more changes were made to master");
         }
     }
 }
