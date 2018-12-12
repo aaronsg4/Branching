fa40a98f49061c961b1427bbd7e5c31efab29fb5
aaronsg4 <aaronsg4@yahoo.com>
2018-12-11 19:20:57 -0500

    Branch off of a branch

--- Branching/Program.cs
+++ Branching/Program.cs
@@ -14,6 +14,7 @@ namespace Branching
             Console.WriteLine("More changes to master");
 
             Console.WriteLine("This is the beginning of the feature 2 branch, which was added after the first feature, and after more changes were made to master");
+            Console.WriteLine("A branch off of a branch?");
         }
     }
 }
